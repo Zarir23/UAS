13b2b40f4785f44c6a1cec761ef7cf01a85a21b7
nofianadita <115968281+nofianadita@users.noreply.github.com>
2023-06-27 15:10:38 +0700

    catatAdonan
    hoho

---
+++ UasPBO Smt2/src/uaspbo/smt2/CatatAdonan.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaspbo.smt2;
+
+/**
+ *
+ * @author ACER
+ */
+public class CatatAdonan {
+    
+     //menghitung berapa takar adonan dari setiap jenis yang perlu dibuat
+    
+    public double Adonanmanis(){
+        TampilOrder obj1 = new TampilOrder();
+        obj1.order();
+        double adonan;
+        
+        
+     
+    }
+    
+}
