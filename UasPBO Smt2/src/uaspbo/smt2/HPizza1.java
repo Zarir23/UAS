ebe1ca091bff2097f9b269c1b8516de6720b1053
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-06-27 15:01:41 +0700

    dita how are you
    hawaryu

---
+++ UasPBO Smt2/src/uaspbo/smt2/HPizza1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaspbo.smt2;
+
+/**
+ *
+ * @author refah
+ */
+public class HPizza1 {
+    BelanjaBahan obj1 = new BelanjaBahan();
+    TampilOrder obj2 = new TampilOrder();
+}
