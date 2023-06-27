4caccf86723b093f33c550cb507abf3487d0738d
nofianadita <115968281+nofianadita@users.noreply.github.com>
2023-06-27 14:59:03 +0700

    bismillah
    hoho

---
+++ UasPBO Smt2/src/uaspbo/smt2/TampilOrder.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaspbo.smt2;
+
+/**
+ *
+ * @author ACER
+ */
+public class TampilOrder {
+    double item;
+    int keju;
+    int coklat;
+    int sosis;
+    int krim;
+    int redbean;
+    int beef;
+    int bombay;
+    public double itemM;
+    public double itemT;
+    public double itemP;
+    
+    public double order(){
+        CatatOrder item1 = new CatatOrder();
+        
+        
+        
+    }
+    
+}
