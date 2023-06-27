a18ea3214ac0294cd2c195321e0d6e7a9269ee5c
Muhammadafiffirdaus01 <afiffirdaus19tkj1@gmail.com>
2023-06-27 16:52:17 +0700

    ralat

--- UasPBO Smt2/src/uaspbo/smt2/CatatOrder.java
+++ UasPBO Smt2/src/uaspbo/smt2/CatatOrder.java
@@ -9,7 +9,76 @@ package uaspbo.smt2;
  * @author Lenovo
  */
 public class CatatOrder {
-    while (pcs > 0) {
+    int doughM;
+    int doughT;
+    int doughP;
+    int pcs;
+    String jenis;
+    private int varian;
+    int keju;
+    int coklat;
+    int redBean;
+    int krim;
+    int sosis;
+    int beef;
+    int bombay;
+    
+    public void setVarian(int varian){
+        if (varian > 0 && varian < 3){
+            this.varian = varian;
+        }
+        else {
+            this.varian = 1;
+        }
+    }
+    
+    private void rotiManis(){
+        doughM = doughM + 50;
+    }
+    
+    private void rotiTawar(){
+        doughT = doughT + 400;
+    }
+    
+    private void pizza(){
+        doughP = doughP + 190;
+    }
+    
+    private void varianM(){
+        switch (varian){
+            case 1 -> {
+                keju = keju + 5;
+                coklat = coklat + 10;
+            }
+            case 2 -> {
+                redBean = redBean + 10;
+                krim = krim + 5;
+            }
+            case 3 -> {
+                keju = keju + 10;
+                sosis = sosis + 10;
+            }
+        }
+    }
+    
+    private void varianT(){
+        switch (varian){
+            case 1 -> coklat = coklat + 80;
+            case 2 -> keju = keju + 80;
+        }
+    }
+    
+    private void varianP(){
+        if (varian == 1){
+            keju = keju + 30;
+            sosis = sosis + 50;
+            beef = beef + 50;
+            bombay = bombay + 30;
+        }
+    }
+    
+    public void catatOrder(){
+        while (pcs > 0) {
             if (jenis == "manis"){
                 this.rotiManis();
                 this.varianM();
@@ -26,5 +95,5 @@ public class CatatOrder {
             
         }
     }
-    
+}
 
