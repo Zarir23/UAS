32896ab6ce0a2a92abead3d44b897a3d8afb5007
Muhammadafiffirdaus01 <afiffirdaus19tkj1@gmail.com>
2023-06-27 13:27:00 +0700

    class BeliBahan, class HManis2
    penambahan class belibahan dan class hmanis2

---
+++ UasPBO Smt2/src/uaspbo/smt2/HManis2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaspbo.smt2;
+
+/**
+ *
+ * @author 62821
+ */
+public class HManis2 {
+    
+}
